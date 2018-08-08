cc857d78ea87e8a336a2cf3c7148464a9967b899
Aziz Yakubov <azizy15@gmail.com>
2018-08-07 17:17:02 -0700

    Implemented advanced login with persistent cookies and remember me box

--- hartl_sample_app/app/models/user.rb
+++ hartl_sample_app/app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
+
   before_save { self.email = email.downcase }
   validates :name, presence: true, length: {maximum: 50}
 
@@ -10,6 +12,11 @@ class User < ApplicationRecord
   validates :password, presence: true, length: { minimum: 6 }
 
 
+  # In this method and ones below, using User.method notation is the same as
+  # self.method. Another way of accomplishing this is by prefixing a line
+  # class << self  . This way, methods don't have to be prefixed with self. or User.
+  # and can just be defined, but it can be a little confusing.
+
   # Returns the hash digest of the given string.
   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
@@ -17,5 +24,27 @@ class User < ApplicationRecord
     BCrypt::Password.create(string, cost: cost)
   end
 
-  
+  # Returns a random token to be used in the remember me checkbox cookie transfer.
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  # Remembers a user in the database for use in persistent sessions.
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  # Returns true if the given token matches the digest. Argument is just a
+  # local variable, not necessarily the same as the attr_accessor listed above
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  # Forgets a user.
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
+
 end
